f48074d231ff708015132f0e5d9ec6ea33b0477d
Awhite246 <awhite@bsd220.org>
2021-11-03 08:22:48 -0500

    Added hitting and standing in test view ( the black-jack game)

--- BlackJack/ContentView.swift
+++ BlackJack/ContentView.swift
@@ -17,160 +17,181 @@ struct ContentView: View {
     @State private var cardAnimate = false
     @State private var charPics = ["","","",""]
     @State private var userNames = ["","","",""]
+    @State private var cardDeck = ["DiamondA","Diamond2","Diamond3","Diamond4","Diamond5","Diamond6","Diamond7","Diamond8","Diamond9","Diamond10","DiamondJ","DiamondQ","DiamondK","ClubA","Club2","Club3","Club4","Club5","Club6","Club7","Club8","Club9","Club10","ClubJ","ClubQ","ClubK","HeartA","Heart2","Heart3","Heart4","Heart5","Heart6","Heart7","Heart8","Heart9","Heart10","HeartJ","HeartQ","HeartK","SpadeA","Spade2","Spade3","Spade4","Spade5","Spade6","Spade7","Spade8","Spade9","Spade10","SpadeJ","SpadeQ","SpadeK"]
     var body: some View {
-        VStack {
-            switch (screen) {
-            case "home":
+        ZStack {
+            VStack {
                 Group {
-                    Text("Black Jack")
-                        .font(.title)
-                        .fontWeight(.bold)
-                        .padding()
-                    Image("Logo")
-                        .resizable()
-                        .frame(width: 300, height: 200, alignment: .center)
-                        .aspectRatio(contentMode: .fit)
-                        .rotationEffect(.init(degrees: logoAnimate ? -8 : 8))
-                        .animation(Animation.linear(duration: 5).delay(0.25).repeatForever())
-                        .onAppear(perform: {
-                            logoAnimate = true
-                        })
-                    Slider(value: $numPlayers, in: 1...4, step: 1.0)
-                        .padding()
-                    Text("Player Count: \(numPlayers, specifier: "%.f")")
-                    Button ("Play Game") {
-                        screen = "game"
-                    }
-                    .font(.title2)
-                    .padding(.top, 70)
-                    .disabled((userNames[0] == "") || (userNames[1] == "" && numPlayers > 1) || (userNames[2] == "" && numPlayers > 2) || (userNames[3]) == "" && numPlayers > 3)
-                    Button ("Character Select") {
-                        screen = "username"
-                        logoAnimate = false
-                    }
-                    .font(.title2)
-                    .padding(25)
-                }
-            case "username":
-                Group {
-                    Text("Character Select")
-                        .font(.title)
-                        .fontWeight(.bold)
-                    Form {
-                        ForEach (0..<4) { i in
-                            HStack {
-                                if Int(numPlayers) > i {
-                                    Button (userNames[i] == "" ? "Player \(i + 1)" : "\(userNames[i])") {
-                                        screen = "character"
-                                        charNum = i
-                                        userName = userNames[charNum]
-                                    }
-                                    .padding()
-                                } else {
-                                    if (userNames[i] == ""){
-                                        Text("Empty")
-                                            .foregroundColor(.gray)
-                                            .padding()
-                                    } else {
-                                        Text(userNames[i])
-                                            .foregroundColor(.gray)
-                                            .padding()
-                                    }
-                                }
-                                Spacer()
-                                Image (charPics[i] == "" ? "User5" : "\(charPics[i])")
-                                    .resizable()
-                                    .frame(width: 40, height: 50, alignment: .center)
-                                    .aspectRatio(contentMode: .fit)
-                                    .grayscale((charPics[i] != "") && (Int(numPlayers) > i) ? 0 : 0.99)
-                                    .blur(radius: (charPics[i] != "") && (Int(numPlayers) > i) ? 0 : 1)
-                            }
-                        }
-                    }
-                    .frame(width: 400, height: 300)
-                    .padding()
-                    HStack {
-                        Image("Back\(cardBack)")
+                    if screen == "game" {
+                        Image("Background1")
+                            .frame(width: 400, height: 570, alignment: .top)
+                            .colorMultiply(.blue)
+                        Image("Background2")
                             .resizable()
-                            .frame(width: 200, height: 300, alignment: .leading)
-                            .aspectRatio(contentMode: .fit)
-                            .rotationEffect(.init(degrees: cardAnimate ? -3 : 1))
-                            .animation(
-                                Animation.linear(duration: 0.1)
-                                    .delay(0.05)
-                            )
-                            .onTapGesture{
-                                cardBack %= 8
-                                cardBack += 1
-                                if cardAnimate {
-                                    cardAnimate = false
-                                } else {
-                                    cardAnimate = true
-                                }
-                            }
-                        Text("Click to change the back of your cards")
+                            .frame(width: 400, height: 300, alignment: .top)
+                            .border(Color.gray, width: 3)
                     }
-                    Button("Back"){
-                        screen = "home"
-                    }
-                    .padding()
-                }
-            case "character" :
-                Group {
-                    Text("Character Select")
-                        .font(.title)
-                        .fontWeight(.bold)
-                        .padding()
-                    LazyVGrid(columns: Array(repeating: GridItem(.fixed(120), spacing: 0),count: 3), spacing: 19, content: {
-                        ForEach(0..<12){ index in
-                            Image("User\(1 + index)")
-                                .resizable()
-                                .frame(width: 45, height: 70, alignment: .center)
-                                .aspectRatio(contentMode: .fit)
-                                .border(Color.gray, width: 1)
-                                .grayscale(charPics.contains("User\(1+index)") ? 0.9 : 0)
-                                .blur(radius: charPics.contains("User\(1+index)") ? 2 : 0)
-                                .onTapGesture {
-                                    if (!charPics.contains("User\(1+index)")){
-                                        charPics[charNum] = "User\(index + 1)"
-                                    }
-                                }
-                        }
-                    })
-                    Text("Selected Character")
-                        .padding(.top, 20)
-                    Image(charPics[charNum] != "" ? charPics[charNum] : "User5")
-                        .resizable()
-                        .frame(width: 45, height: 70, alignment: .center)
-                        .aspectRatio(contentMode: .fit)
-                        .border(Color.white, width: 1)
-                        .grayscale(charPics[charNum] != "" ? 0 : 0.9)
-                        .blur(radius: charPics[charNum] != "" ? 0 : 2)
-                    TextField("Username", text: $userName)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .multilineTextAlignment(.center)
-                        .frame(width: 200, height: 30, alignment: .center)
-                        .font(.body)
-                        .padding()
-                    Spacer()
-                    Button("Done") {
-                        userNames[charNum] = userName
-                        screen = "username"
-                    }
-                    .disabled(charPics[charNum] == "" || userName == "")
-                    .padding()
-                }
-            case "game":
-                Group {
-                    Text("test")
-                }
-            default:
-                Group {
-                    Text("test")
                 }
             }
+            VStack {
+                switch (screen) {
+                case "home":
+                    Group {
+                        Text("Black Jack")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .padding()
+                        Image("Logo")
+                            .resizable()
+                            .frame(width: 300, height: 200, alignment: .center)
+                            .aspectRatio(contentMode: .fit)
+                            .rotationEffect(.init(degrees: logoAnimate ? -8 : 8))
+                            .animation(Animation.linear(duration: 5).delay(0.25).repeatForever())
+                            .onAppear(perform: {
+                                logoAnimate = true
+                            })
+                        
+                        Slider(value: $numPlayers, in: 1...4, step: 1.0)
+                            .padding()
+                        Text("Player Count: \(numPlayers, specifier: "%.f")")
+                        Button ("Play Game") {
+                            screen = "game"
+                        }
+                        .font(.title2)
+                        .padding(.top, 70)
+                        .disabled((userNames[0] == "") || (userNames[1] == "" && numPlayers > 1) || (userNames[2] == "" && numPlayers > 2) || (userNames[3]) == "" && numPlayers > 3)
+                        Button ("Character Select") {
+                            screen = "username"
+                            logoAnimate = false
+                        }
+                        .font(.title2)
+                        .padding(25)
+                    }
+                case "username":
+                    Group {
+                        Text("Character Select")
+                            .font(.title)
+                            .fontWeight(.bold)
+                        Form {
+                            ForEach (0..<4) { i in
+                                HStack {
+                                    if Int(numPlayers) > i {
+                                        Button (userNames[i] == "" ? "Player \(i + 1)" : "\(userNames[i])") {
+                                            screen = "character"
+                                            charNum = i
+                                            userName = userNames[charNum]
+                                        }
+                                        .padding()
+                                    } else {
+                                        if (userNames[i] == ""){
+                                            Text("Empty")
+                                                .foregroundColor(.gray)
+                                                .padding()
+                                        } else {
+                                            Text(userNames[i])
+                                                .foregroundColor(.gray)
+                                                .padding()
+                                        }
+                                    }
+                                    Spacer()
+                                    Image (charPics[i] == "" ? "User5" : "\(charPics[i])")
+                                        .resizable()
+                                        .frame(width: 40, height: 50, alignment: .center)
+                                        .aspectRatio(contentMode: .fit)
+                                        .grayscale((charPics[i] != "") && (Int(numPlayers) > i) ? 0 : 0.99)
+                                        .blur(radius: (charPics[i] != "") && (Int(numPlayers) > i) ? 0 : 1)
+                                }
+                            }
+                        }
+                        .frame(width: 400, height: 300)
+                        .padding()
+                        HStack {
+                            Image("Back\(cardBack)")
+                                .resizable()
+                                .frame(width: 200, height: 300, alignment: .leading)
+                                .aspectRatio(contentMode: .fit)
+                                .rotationEffect(.init(degrees: cardAnimate ? -3 : 1))
+                                .animation(
+                                    Animation.linear(duration: 0.1)
+                                        .delay(0.05)
+                                )
+                                .onTapGesture{
+                                    cardBack %= 8
+                                    cardBack += 1
+                                    if cardAnimate {
+                                        cardAnimate = false
+                                    } else {
+                                        cardAnimate = true
+                                    }
+                                }
+                            Text("Click to change the back of your cards")
+                        }
+                        .frame(width: 390, alignment: .center)
+                        Button("Back"){
+                            screen = "home"
+                        }
+                        .padding()
+                    }
+                case "character" :
+                    Group {
+                        Text("Character Select")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .padding()
+                        LazyVGrid(columns: Array(repeating: GridItem(.fixed(120), spacing: 0),count: 3), spacing: 19, content: {
+                            ForEach(0..<12){ index in
+                                Image("User\(1 + index)")
+                                    .resizable()
+                                    .frame(width: 45, height: 70, alignment: .center)
+                                    .aspectRatio(contentMode: .fit)
+                                    .border(Color.gray, width: 1)
+                                    .grayscale(charPics.contains("User\(1+index)") ? 0.9 : 0)
+                                    .blur(radius: charPics.contains("User\(1+index)") ? 2 : 0)
+                                    .onTapGesture {
+                                        if (!charPics.contains("User\(1+index)")){
+                                            charPics[charNum] = "User\(index + 1)"
+                                        }
+                                    }
+                            }
+                        })
+                        Text("Selected Character")
+                            .padding(.top, 20)
+                        Image(charPics[charNum] != "" ? charPics[charNum] : "User5")
+                            .resizable()
+                            .frame(width: 45, height: 70, alignment: .center)
+                            .aspectRatio(contentMode: .fit)
+                            .border(Color.white, width: 1)
+                            .grayscale(charPics[charNum] != "" ? 0 : 0.9)
+                            .blur(radius: charPics[charNum] != "" ? 0 : 2)
+                        TextField("Username", text: $userName)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .multilineTextAlignment(.center)
+                            .frame(width: 200, height: 30, alignment: .center)
+                            .font(.body)
+                            .padding()
+                        Spacer()
+                        Button("Done") {
+                            userNames[charNum] = userName
+                            screen = "username"
+                        }
+                        .disabled(charPics[charNum] == "" || userName == "")
+                        .padding()
+                    }
+                case "game":
+                    Group {
+                        Text("test")
+                    }
+                default:
+                    Group {
+                        Text("test")
+                    }
+                }
+            }
+            .onAppear(perform: {
+                cardDeck = cardDeck.shuffled()
+            })
+            .preferredColorScheme(.dark)
         }
-        .preferredColorScheme(.dark)
     }
 }
 
--- BlackJack/TestView.swift
+++ BlackJack/TestView.swift
@@ -18,30 +18,131 @@ struct TestView: View {
     @State private var charPics = ["","","",""]
     @State private var userNames = ["","","",""]
     @State private var cardDeck = ["DiamondA","Diamond2","Diamond3","Diamond4","Diamond5","Diamond6","Diamond7","Diamond8","Diamond9","Diamond10","DiamondJ","DiamondQ","DiamondK","ClubA","Club2","Club3","Club4","Club5","Club6","Club7","Club8","Club9","Club10","ClubJ","ClubQ","ClubK","HeartA","Heart2","Heart3","Heart4","Heart5","Heart6","Heart7","Heart8","Heart9","Heart10","HeartJ","HeartQ","HeartK","SpadeA","Spade2","Spade3","Spade4","Spade5","Spade6","Spade7","Spade8","Spade9","Spade10","SpadeJ","SpadeQ","SpadeK"]
-    @State private var cardDeckRandom = ["DiamondA","Diamond2","Diamond3","Diamond4","Diamond5","Diamond6","Diamond7","Diamond8","Diamond9","Diamond10","DiamondJ","DiamondQ","DiamondK","ClubA","Club2","Club3","Club4","Club5","Club6","Club7","Club8","Club9","Club10","ClubJ","ClubQ","ClubK","HeartA","Heart2","Heart3","Heart4","Heart5","Heart6","Heart7","Heart8","Heart9","Heart10","HeartJ","HeartQ","HeartK","SpadeA","Spade2","Spade3","Spade4","Spade5","Spade6","Spade7","Spade8","Spade9","Spade10","SpadeJ","SpadeQ","SpadeK"]
-    @State private var switchy = false
+    @State private var count = 0
+    @State private var currentPlayer = 0
+    @State private var player1 = [String]()
+    @State private var player2 = [String]()
+    @State private var player3 = [String]()
+    @State private var player4 = [String]()
     var body : some View {
-        VStack {
-            ForEach (0..<5) { i in
+        ZStack {
+            VStack {
+                Image("Background1")
+                    .frame(width: 400, height: 550, alignment: .top)
+                    .colorMultiply(.blue)
+                Image("Background2")
+                    .resizable()
+                    .frame(width: 400, height: 290, alignment: .top)
+                    .border(Color.gray, width: 3)
+            }
+            VStack {
                 HStack {
-                    Text("Card \(cardDeckRandom[i])\t")
-                        .font(.title)
-                        .padding()
-                        .frame(alignment: .leading)
-                    Image("\(cardDeckRandom[i])")
-                        .resizable()
-                        .frame(width: 75, height: 110, alignment: .trailing)
-                        .aspectRatio(contentMode: .fit)
-                        .padding()
+                    ForEach(0..<8){
+                        Image(cardDeck[$0])
+                            .resizable()
+                            .frame(width: 40, height: 60, alignment: .center)
+                            .aspectRatio(contentMode: .fit)
+                            .border(count == $0 ? Color.red : Color.clear, width: 2)
+                        
+                    }
                 }
+                .padding(.top, 300)
+                Text("\(count)")
+                Text("\(currentPlayer)")
+                HStack {
+                    ForEach(player1.indices, id: \.self) { index in
+                        Text("\(player1[index])")
+                    }
+                }
+                Spacer()
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack {
+                        ForEach(player1.indices, id: \.self) { index in
+                            Image(player1[index])
+                                .resizable()
+                                .frame(width: 100, height: 150, alignment: .center)
+                                .aspectRatio(contentMode: .fit)
+                        }
+                        
+                    }
+                }
+                .frame(width: 350, height: 150, alignment: .center)
+                HStack {
+                    Button {
+                        addPlayerCard(current: currentPlayer, text: cardDeck[count])
+                        count += 1
+                        count %= 52
+                        currentPlayer += 1
+                        currentPlayer %= 4
+                    } label: {
+                        Text ("Hit")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .padding()
+                    }
+                    .disabled(checkPlayer(current: currentPlayer))
+                    Button {
+                        addPlayerCard(current: currentPlayer, text: "Stand")
+                        currentPlayer += 1
+                        currentPlayer %= 4
+                    } label: {
+                        Text ("Stand")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .padding()
+                    }
+                }
+                .padding(.bottom)
             }
-            .frame(width: 500, height: 110, alignment: .center)
-            Button ("Randomize") {
-                cardDeckRandom = cardDeck.shuffled()
-            }
-            .padding()
+            .onAppear(perform: {
+                cardDeck = cardDeck.shuffled()
+            })
+            .preferredColorScheme(.dark)
         }
-        .preferredColorScheme(.dark)
+    }
+    func addPlayerCard (current : Int, text : Any) {
+        switch current {
+        case 0:
+            player1.append("\(text)")
+        case 1:
+            player2.append("\(text)")
+        case 2:
+            player3.append("\(text)")
+        case 3:
+            player4.append("\(text)")
+        default:
+            break
+        }
+    }
+    func checkPlayer (current : Int) -> Bool {
+        switch current {
+        case 0:
+            if player1.last == "Stand" {
+                return true
+            }
+        case 1:
+            if player2.last == "Stand" {
+                return true
+            }
+        case 2:
+            if player3.last == "Stand" {
+                return true
+            }
+        case 3:
+            if player4.last == "Stand" {
+                return true
+            }
+        default:
+            break
+        }
+        return false
+    }
+}
+struct Background: View {
+    var body : some View {
+        Image("BackGround")
     }
 }
 struct TestView_Previews: PreviewProvider {
